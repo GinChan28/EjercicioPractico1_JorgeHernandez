755886bd7001fe200ce0b23a6660975dd719c9db
GinChan28 <jhernandez80996@ufide.ac.cr>
2025-02-26 22:44:55 -0600

    Add files via upload

--- Farmaciaa/src/main/java/com/farmacia/controllers/ProductoController.java
+++ Farmaciaa/src/main/java/com/farmacia/controllers/ProductoController.java
@@ -2,19 +2,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.farmacia.controllers;
 
 import com.farmacia.domain.Producto;
 import com.farmacia.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @RestController
+@Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    @Autowired
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/Productos")
+    public String productos() {
+        return "Productos";
+    }
+
+    @GetMapping("/Sugerencias")
+    public String sugerencias() {
+        return "Sugerencias";
+    }
+
     @Autowired
     private ProductoService productoService;
 
@@ -37,4 +55,5 @@ public class ProductoController {
     public Producto obtenerProductoPorId(@PathVariable Long id) {
         return productoService.obtenerProductoPorId(id);
     }
+
 }
